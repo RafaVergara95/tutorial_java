9c6aa93fd594a2e0cb3915f63942ef0440037d25
RafaVergara95 <rafa95@live.cl>
2022-03-24 18:40:35 -0300

    tutorial 37

---
+++ source/Main.java
@@ -0,0 +1,16 @@
+package source;
+
+public class Main {
+    
+    public static void main(String args[]){
+
+        t37 arbol = new t37();
+        arbol.insertar(5, null);
+        arbol.insertar(3, null);
+        arbol.insertar(1, null);
+        arbol.insertar(2, null);
+        arbol.insertar(7, null);
+        arbol.insertar(6, null);
+    }
+
+}
---
+++ source/t37.java
@@ -0,0 +1,51 @@
+package source;
+
+public class t37 {
+
+    Nodo raiz;
+
+    public t37(){
+        raiz = null;
+    }
+    public void insertar(int key, Object valor){
+        Nodo n = new Nodo(key);
+        n.valor = valor;
+
+        if(raiz == null){
+            raiz = n;
+        }else{
+            Nodo temporal = raiz;
+            while(temporal != null){
+                n.p = temporal;
+                if(n.llave >= temporal.llave){
+                    temporal = temporal.der;
+
+                }else{
+                    temporal = temporal.izq;
+                }
+            }
+            if (n.p.llave < n.p.llave){
+                n.p.izq = n;
+            }else{
+                n.p.der = n;
+            }
+        }
+    }
+
+    private class Nodo{
+
+        public Nodo p; //padre
+        public Nodo der;
+        public Nodo izq;
+        public int llave;
+        public Object valor;
+
+        public Nodo(int key){
+            llave = key;
+            der= null;
+            izq = null;
+            p = null;
+            valor = null;
+        }
+    }
+}
