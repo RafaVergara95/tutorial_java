35c861b7784c627b46f90ffcde7b3140076f91c5
RafaVergara95 <rafa95@live.cl>
2022-03-29 20:26:23 -0300

    tutorial 7

---
+++ t7.java
@@ -0,0 +1,18 @@
+
+
+import java.util.Scanner;
+
+public class t7 {
+    public static void main(String[] args){
+        Scanner escan = new Scanner(System.in);
+
+        System.out.println("Dame un numero");
+
+        int x;
+        x = escan.nextInt();
+
+        System.out.println("El numero es : " + x);
+
+    }
+    
+}
