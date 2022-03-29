87ba4d899d92595312104b85b982077ff898d512
RafaVergara95 <rafa95@live.cl>
2022-03-29 20:18:52 -0300

    tutorial 16

---
+++ t16.java
@@ -0,0 +1,22 @@
+package source;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main (String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Dame una palabra:");
+        String palabra;
+        palabra = scanner.next();
+
+        char p[] = palabra.toCharArray();
+
+        for(int count = 0; count < y.length; count++) {
+            
+            System.out.println(y[count]);
+        }
+    }
+}
